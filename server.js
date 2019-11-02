7672809419c4aa1f7c790f91897280d7963eab03
Jasper Morgal <jpmorgal8@gmail.com>
2019-11-02 10:32:26 -0700

    Added my name to server.js file

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express()
 console.log("On branch test")
 app.get('/', (req, res) => res.send('Hello World!'))
 
+console.log("Jasper Morgal")
 console.log("creating diff!!")
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
